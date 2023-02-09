9169cea2fd257632646c6a7892cfd34084116874
Ognjen Vujicic <ognjenvuj@gmail.com>
2023-02-10 00:21:31 +0100

    create gitignore file

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
+
     let amount = 0;
     const userDeposit = document.getElementById("userDeposit");
     const depositButton = document.getElementById("depositButton");
     const userWithdraw = document.getElementById("userWithdraw");
     const withdrawButton = document.getElementById("withdrawButton");
     const balanceDisplay = document.getElementById("Preview");
+    let statusDisplay = document.getElementById("status");
 
     function updateBalance(newValue) {
-        balanceDisplay.innerHTML = newValue
+        balanceDisplay.innerHTML = newValue;
     }
 
     let account = {
@@ -25,64 +27,76 @@ document.addEventListener("DOMContentLoaded", function() {
             return this.balance;
         }
     };
-
     balanceDisplay.innerHTML = account.balance;
 
+
     depositButton.addEventListener("click", function() { 
+        
         amount = +userDeposit.value;
 
-        let newBalance = account.deposit();
+        if (amount < 0.1) {
+            alert("You can only deposit above $0.01 .")
+        }
+        else {
+            let newBalance = account.deposit();
 
-        updateBalance(newBalance);
+            updateBalance(newBalance);
+        }
+        if (account.balance <= 0) {
+            statusDisplay.innerHTML = "Red";
+            console.log("Red");
+    
+        } 
+        else if (account.balance < 1000000) {
+            statusDisplay.innerHTML = "Active";
+            console.log("Active");
+    
+        }
+        else {
+            statusDisplay.innerHTML = "Gold";
+            console.log("Gold");
+    
+        }
     });
-
-
+    
     withdrawButton.addEventListener("click", function() { 
         amount = +userWithdraw.value;
         
-        let newBalance = account.withdraw(); 
+        if(account.balance - amount < -1000) {
+            alert("Your account balance is less than $-1000");
+        }
+        else if (account.balance > 1000000) {
+            amount = amount + (amount * 1 / 100);
+            let newBalance = account.withdraw();
+            updateBalance(newBalance);
+        }
+        else if (amount < 0.1 || amount > 1000) {
+            alert("You can only withdraw between 0.1 and 1000")
+        }
+        else {
+            let newBalance = account.withdraw(); 
 
-        updateBalance(newBalance);
+            updateBalance(newBalance);
+        }
+
+        if (account.balance <= 0) {
+            statusDisplay.innerHTML = "Red";
+            console.log("Red");
+        
+        } 
+        else if (account.balance < 1000000) {
+            statusDisplay.innerHTML = "Active";
+            console.log("Active");
+        
+        }
+        else {
+            statusDisplay.innerHTML = "Gold";
+            console.log("Gold");
+        
+        }
+        
     });
-
-
-
-    /*function promptMeToWithdraw()   {
-        thisAmount = prompt('Enter the amount to withdraw'); 
-        let amount = parseInt(thisAmount);
-        return account.withdraw(amount);
-
-    }
-
-
-
-    function promptMeToDeposit()  {
-        thisAmount = prompt('Enter the amount to deposit'); 
-        let amount = parseInt(thisAmount);
-        return account.deposit(amount);
-
-    } 
-
-
-
-    document.getElementById('withdrawButton').addEventListener('click', promptMeToWithdraw);
-    document.getElementById('Preview').innerHTML = "Balance: <br>" + account.balance;
-
-    document.getElementById('depositButton').addEventListener('click', promptMeToDeposit);
-    document.getElementById('Preview').innerHTML = "Balance: <br>" + account.balance; 
-    */
-
-
-    if (account.balance <= 0) {
-        document.getElementById('status').innerHTML = "Red";
-
-    } 
-    else if (account.balance > 0 && account.balance < 1000000) {
-        document.getElementById('status').innerHTML = "Active";
-
-    }
-    else if (account.balance >= 1000000) {
-        document.getElementById('status').innerHTML = "Gold";
-
-    }
 });
+
+   
+    
